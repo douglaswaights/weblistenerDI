ab9aa69758e650ca36e90640bb26006895309be8
Douglas Waights <douglas.waights@cegal.com>
2016-06-06 17:25:36 +0200

    issue

---
+++ src/weblistener/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Net.Http.Server;
+
+namespace weblistener
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+               .UseWebListener(options =>
+               {
+                 options.Listener.AuthenticationManager.AuthenticationSchemes = AuthenticationSchemes.NTLM;
+               })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ src/weblistener/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Autofac;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Autofac.Extensions.DependencyInjection;
+
+namespace weblistener
+{
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+    public IServiceProvider ConfigureServices(IServiceCollection services)
+    {
+
+      // Add Autofac
+      var containerBuilder = new ContainerBuilder();
+      containerBuilder.Populate(services);
+      var container = containerBuilder.Build();
+      return container.Resolve<IServiceProvider>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app)
+    {
+      app.Run(async (context) =>
+      {
+        var claimsPrinciple = context.User;
+        string name = claimsPrinciple.Identity.Name;
+        await context.Response.WriteAsync("Hello World!");
+      });
+    }
+  }
+}
